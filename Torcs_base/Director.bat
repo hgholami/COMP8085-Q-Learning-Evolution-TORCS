8884d4bafcafc7c916527fe3c6660dc9e3d76b97
Henry723 <henryzhang1020@gmail.com>
2022-04-11 17:20:30 -0700

    Director modified for mutation rate experiment

--- Torcs_base/Director.bat
+++ Torcs_base/Director.bat
@@ -1,10 +1,10 @@
 @echo off
-set /a ep_num=5
+set /a ep_num=10
 set race_config_name=quickrace_CG1_1Lap.xml
 
-set /a pop_size=4
-set /a gen_num=2
-set /a elite_size=3
+set /a pop_size=2
+set /a gen_num=50
+set /a elite_size=2
 
 set /a curr_ind=1
 set /a curr_gen=1
